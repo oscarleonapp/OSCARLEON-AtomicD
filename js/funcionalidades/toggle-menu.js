4a7433b61c7097b9b1d860e143815c63571185d8
OSCARLEON <info@oscarleon.app>
2025-03-07 17:25:21 -0600

    barra de navegacion

--- js/funcionalidades/toggle-menu.js
+++ js/funcionalidades/toggle-menu.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+// Rotar el DropDown
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+  dropdown.addEventListener('mouseenter', () => {
+      dropdown.querySelector('i').classList.add('rotated');
+  });
+  dropdown.addEventListener('mouseleave', () => {
+      dropdown.querySelector('i').classList.remove('rotated');
+  });
+});
